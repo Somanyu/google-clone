71d7883387aae92efad9786be59695aad66672d4
Somanyu <somanyu.03samal@gmail.com>
2022-01-21 07:19:29 +0530

    directory changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SearchPage from './SearchPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
